d33f538a7907751219a52e17e15605e2c7f1cd0b
HelperImmerse <helperimmerse@gmail.com>
2022-11-27 10:23:24 +0530

    my chat application

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const { dirname } = require("path");
+const app = express()
+const http = require('http').createServer(app)
+
+const PORT = process.env.PORT || 3000
+
+http.listen(PORT, ()=> {
+    console.log(`app running on this ${PORT}`)
+})
+
+app.use(express.static(__dirname + '/Public'))
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/index.html')
+})
+
+// socket 
+
+const io = require('socket.io') (http)
+
+io.on('connection', (socket)=> {
+console.log('connected')
+socket.on('message', (msg)=> {
+    socket.broadcast.emit('message', msg)
+})
+
+})
